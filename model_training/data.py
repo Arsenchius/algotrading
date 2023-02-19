1afa7df7b9cc69fc796f65d9e278e5ec498f01fc
Arsenchius <arsensdsdsdsd@gmail.com>
2023-02-20 01:19:24 +0300

    model hyperparams tuning with optuna

---
+++ model_training/data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from binance import Client
+import ta
+import sys
+import tqdm
+
+sys.path.insert(
+    1,
+    "/Users/arsenchik/Desktop/dipploma/machine_learning_in_hft/algotrading/backtesting",
+)
+from backtest import *
+from strategies import *
+from config import *
+
+
+FEATURES = ['Open',
+    'High',
+    'Low',
+    'Volume',
+    'RSI_2',
+    'SMA_20',
+    'RSI_4',
+    'SMA_40',
+    'RSI_6',
+    'SMA_60',
+    'RSI_8',
+    'SMA_80']
+TARGET = 'Target'
+
+
+def get_data(symbol, time_frame=config.INTERVAL_1MINUTE, end_date="1000 hours ago UTC"):
+    client = Client()
+    df = pd.DataFrame(client.get_historical_klines(symbol, time_frame, end_date))
+
+    df = df.iloc[:, 0:6]
+    df.columns = ["Time", "Open", "High", "Low", "Close", "Volume"]
+    df.set_index("Time", inplace=True)
+    df.index = pd.to_datetime(df.index, unit="ms")
+    df = df.astype(float)
+    return df
+
+
+def get_features():
+    pass
+
---
+++ model_training/model.py
@@ -0,0 +1,217 @@
+import lightgbm as lgb
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+
+# from catboost import CatBoostRegressor, CatBoostClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import (
+    log_loss,
+    roc_auc_score,
+    f1_score,
+    mean_squared_error,
+    accuracy_score,
+    r2_score,
+    mean_absolute_error,
+)
+from data import *
+from optuna.integration import LightGBMPruningCallback
+import optuna
+from params import *
+import yaml
+from tqdm import tqdm
+import ta
+import os
+
+
+
+def get_metrics():
+    metrics_config_file = "metrics.yaml"
+    try:
+        with open(metrics_config_file, "r") as file:
+            metrics = yaml.safe_load(file)
+    except Exception as e:
+        print("Error reading the config file")
+    return metrics
+
+def Objective(model, score_metric, number_of_splits=5, test_size=5*24*60):
+        """
+        Define an objective function to be minimized or maximized.
+        type:
+        - int: integer
+        - uni: a uniform float sampling
+        - log: a uniform float sampling on log scale
+        - dis: a discretized uniform float sampling
+        - cat: category; ('auto', 'mode1', 'mode2', )
+
+        """
+        method_names = {
+            "int": "suggest_int",
+            "uni": "suggest_uniform",
+            "log": "suggest_loguniform",
+            "dis": "suggest_discrete_uniform",
+            "cat": "suggest_categorical",
+            "float": "suggest_float",
+        }
+        model_params = {
+            model.model_name: {
+                key: (
+                    method_names.get(val[0]),
+                    ("{}_{}".format(model.model_name, key), *val[1:]),
+                )
+                if type(val) is tuple else val
+                for key, val in model.params.items()
+            }
+        }
+
+        print(model_params)
+
+        def _objective(trial):
+
+            # Invoke suggest methods of a Trial object to generate hyperparameters.
+            params = {}
+            for key, val in model_params[model.model_name].items():
+                if type(val) is tuple:
+                    if val[0].split('_')[1] == "float" and len(val[1]) > 3:
+                        values = list(val[1])
+                        params[key] = getattr(trial, val[0])(*tuple(values[:-1]), step=values[-1])
+                    else:
+                        params[key] = getattr(trial, val[0])(*val[1])
+                else:
+                    params[key] = val
+
+            #  evaluation
+            tss = TimeSeriesSplit(n_splits=number_of_splits, test_size=test_size)
+            model.df = model.df.sort_index()
+            predictions = []
+            scores = []
+
+            for train_idx, val_idx in tss.split(model.df):
+                train = model.df.iloc[train_idx]
+                test = model.df.iloc[val_idx]
+
+                X_train = train[FEATURES]
+                y_train = train[TARGET]
+
+                X_test = test[FEATURES]
+                y_test = test[TARGET]
+
+                model_loaded = model.model(**params)
+
+                model_loaded.fit(
+                    X_train,
+                    y_train,
+                    eval_set=[(X_test, y_test)],
+                    early_stopping_rounds=100,
+                )
+
+                y_pred = model_loaded.predict(X_test)
+                predictions.append(y_pred)
+                scores.append(score_metric(y_test, y_pred))
+
+            return np.mean(scores)
+
+        return _objective
+
+class Model:
+    def __init__(self, df, model_name, task_type):
+        self.df = df
+        self.model_name = model_name
+        self.task_type = task_type
+        if self.task_type == "classification":
+            if model_name == "LightGBM":
+                self.model = lgb.LGBMClassifier
+            elif model_name == "XGBoost":
+                self.model = xgb.XGBClassifier
+            # elif model_name == "Catboost":
+            # self.model = CatBoostClassifier()
+            # elif model_name == "Linear":
+            #     self.model =
+            else:
+                print("wrong type of model")
+        elif self.task_type == "regression":
+            if model_name == "LightGBM":
+                self.model = lgb.LGBMRegressor
+            elif model_name == "XGBoost":
+                self.model = xgb.XGBRegressor
+            # elif model_name == "Catboost":
+            # self.model = CatBoostRegressor()
+            else:
+                print("wrong type of model")
+        else:
+            print("wrong type of task")
+
+        self.params = param_grid[model_name]
+
+    def indicators_calc(self):
+        for i in range(2, 10, 2):
+            self.df[f"RSI_{i}"] = ta.momentum.rsi(self.df["Close"], window=i)
+            self.df[f"SMA_{i*10}"] = self.df["Close"].rolling(i * 10).mean()
+        # df['MACD'] = ta.trend.macd_diff(df['Close'])
+        self.df.dropna(inplace=True)
+
+    def create_target(self, period=1):
+        self.df["Target"] = self.df["Close"].shift(-period) - self.df["Close"]
+        self.df = self.df[:-period]
+        if self.task_type == "classification":
+            self.df["Target"] = np.where(self.df["Target"] > 0, 1, 0)
+
+    # def objective(
+    #     self, trial, param_grid, score_metric, number_of_splits=5, test_size=5 * 24 * 60
+    # ):
+
+    #     tss = TimeSeriesSplit(n_splits=number_of_splits, test_size=test_size)
+    #     self.df = self.df.sort_index()
+    #     predictions = []
+    #     scores = []
+
+    #     for train_idx, val_idx in tss.split(self.df):
+    #         train = self.df.iloc[train_idx]
+    #         test = self.df.iloc[val_idx]
+
+    #         X_train = train[FEATURES]
+    #         y_train = train[TARGET]
+
+    #         X_test = test[FEATURES]
+    #         y_test = test[TARGET]
+
+    #         self.model.fit(
+    #             X_train,
+    #             y_train,
+    #             eval_set=[(X_test, y_test)],
+    #             early_stopping_rounds=100,
+    #         )
+
+    #         y_pred = self.model.predict(X_test)
+    #         predictions.append(y_pred)
+    #         scores.append(score_metric(y_test, y_pred))
+
+    #     return np.mean(scores)
+
+    def optimize(self, output_dir_path):
+        metrics = get_metrics()
+        if not os.path.exists(output_dir_path):
+            os.mkdir(output_dir_path)
+
+        full_dir_output_path = os.path.join(output_dir_path, f"res_{self.task_type}/")
+
+        if not os.path.exists(full_dir_output_path):
+            os.mkdir(full_dir_output_path)
+
+        for item in tqdm(metrics[self.task_type]):
+            direction = metrics[item]["objective"]
+            direction = "minimize" if direction == "min" else "maximize"
+            study_name = f"{self.task_type} {self.model_name} with metric - {item}"
+            study = optuna.create_study(direction=direction, study_name=study_name)
+            # func = lambda trial: Objective(self, metric_grid[item])
+            func = Objective(self, metric_grid[item])
+            study.optimize(func, n_trials=10)
+            results = study.best_params
+            results["best_value"] = study.best_value
+            results["metric"] = metrics[item]['abbr']
+            full_path = os.path.join(
+                full_dir_output_path, f"{self.model_name}_best_parameters_{item}.yaml"
+            )
+            with open(full_path, "w") as outfile:
+                yaml.dump(results, outfile, default_flow_style=False)
