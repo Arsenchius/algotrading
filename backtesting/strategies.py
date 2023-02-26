b8c0c0adc2bde588d08eec2e8990c120b1acb136
Arsenchius <arsensdsdsdsd@gmail.com>
2023-02-26 01:27:30 +0000

    new strategies testing

--- backtesting/strategies.py
+++ backtesting/strategies.py
@@ -11,6 +11,7 @@ from experiments import EXPERIMENT_ID_TO_FEATURES
 from dataset import Data
 import numpy as np
 import warnings
+import pandas as pd
 
 warnings.filterwarnings("ignore")
 
@@ -93,11 +94,14 @@ class Basic(Strategy):
 
 class BinaryBasic(Strategy):
 
+    price_delta = .004
+
     def init(self):
         self.session = rt.InferenceSession(binary_model_path)
         self.input_name = self.session.get_inputs()[0].name
+        self.output_name = self.session.get_outputs()[1].name
         self.features = features_24
-        self.price_delta = .005 # 0.05%
+        # self.price_delta = price_delta # 0.05%
 
         # Plot y for inspection
         # self.I(get_y, self.data.df, name='y_true')
@@ -111,10 +115,10 @@ class BinaryBasic(Strategy):
         # set a take profit and stop loss parameters:
         price = self.data.Close[-1]
         upper, lower = price * (1 + np.r_[1, -1]*self.price_delta)
+        high, low, close = self.data.High, self.data.Low, self.data.Close
+        current_time = self.data.index[-1]
 
         # data preparation:
-        # print(self.data.df[-1:])
-        # print(self.data.df.drop(['Close_Time'], axis=1))
         data_for_predict = Data(self.data.df)
         data_for_predict.create_features()
 
@@ -122,8 +126,11 @@ class BinaryBasic(Strategy):
 
         # create a model prediction:
         # Forecast the next movement
-        forecast = self.session.run( None, {self.input_name: data_for_predict.df[self.features][-1:].values.astype(np.float32)})[0][0]
-
+        forecast_prob = self.session.run( [self.output_name], {self.input_name: data_for_predict.df[self.features][-1:].values.astype(np.float32)})[0][-1]
+        if forecast_prob[1] >= 0.45:
+            forecast = 1
+        else:
+            forecast = 0
         # Update the plotted "forecast" indicator
         self.forecasts[-1] = forecast
 
@@ -131,10 +138,17 @@ class BinaryBasic(Strategy):
         if forecast == 1:
             if self.position.is_short or not self.position:
                 self.position.close()
-                # self.buy(tp=upper, sl=lower)
-                self.buy()
+                self.buy(size=.1,tp=upper, sl=lower)
+                # self.buy()
         elif forecast == 0:
             if self.position.is_long or not self.position:
                 self.position.close()
-                # self.sell(tp=lower, sl=upper)
-                self.sell()
+                self.sell(size=.1,tp=lower, sl=upper)
+                # self.sell()
+
+        for trade in self.trades:
+            if current_time - trade.entry_time > pd.Timedelta('2 minutes'):
+                if trade.is_long:
+                    trade.sl = max(trade.sl, low)
+                else:
+                    trade.sl = min(trade.sl, high)
